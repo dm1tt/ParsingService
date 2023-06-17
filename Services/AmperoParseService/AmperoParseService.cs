7a1f3b27a1a825a44858757a7d8382e69cf1df75
Dmitry <Diman_krasnov02@mail.ru>
2023-06-17 18:17:50 +0300

    спарсил цену и ссылку, немного оптимизировал

--- Services/AmperoParseService/AmperoParseService.cs
+++ Services/AmperoParseService/AmperoParseService.cs
@@ -1,4 +1,5 @@
 
+using System.Text.RegularExpressions;
 using AngleSharp.Dom;
 using AngleSharp.Html.Parser;
 using ParsingService.Models;
@@ -12,15 +13,47 @@ public class AmperoParseService : IAmperoParseService
     }
     public async Task<List<Product>> GetProdictListHtml(string url, string queryMessage)
     {
-        var productList = new List<string>();
-        string html = await _httpClient.GetStringAsync(url + queryMessage);
+        var productList = new List<Product>();
 
+        string html = await _httpClient.GetStringAsync(url + queryMessage);
         var parser = new HtmlParser();
         var doc = parser.ParseDocument(html);
 
         int totalResults = GetTotalResults(doc);
-        var productLinks = doc.QuerySelectorAll("a.search-link").Select(link => link.Text());
-        productList.AddRange(productLinks);
+
+        var productNodes = doc.QuerySelectorAll(".mse2-row");
+
+        foreach (var productNode in productNodes)
+        {
+            var productName = productNode.QuerySelector(".search-link")?.TextContent.Trim();
+            var priceNode = productNode.QuerySelector("div.col-md-10.col-sm-8.col-xs-8.bottommargin-xs");
+            var priceText = priceNode?.TextContent;
+            if (priceText != null)
+            {
+                var priceMatch = Regex.Match(priceText, @"Цена:\s*(\d+)");
+                if (priceMatch.Success)
+                {
+                    var priceValue = priceMatch.Groups[1].Value;
+                }
+            }
+
+            var productLink = productNode.QuerySelector("a.search-link")?.GetAttribute("href");
+
+            if (!string.IsNullOrEmpty(productName) && !string.IsNullOrEmpty(productLink))
+            {
+                var product = new Product
+                {
+                    ProductId = Guid.NewGuid().ToString(),
+                    Name = productName,
+                    Description = null,
+                    Image = null,
+                    Link = productLink,
+                    ProductType = null
+                };
+
+                productList.Add(product);
+            }
+        }
 
         int numberOfPage = 2;
         
@@ -28,18 +61,39 @@ public class AmperoParseService : IAmperoParseService
         {
             html = await _httpClient.GetStringAsync(url + queryMessage + $"&page={numberOfPage}");
             doc = parser.ParseDocument(html);
-            productLinks = doc.QuerySelectorAll("a.search-link").Select(link => link.Text());
-        
-            productList.AddRange(productLinks);
+            productNodes = doc.QuerySelectorAll(".mse2-row");
+
+            foreach (var productNode in productNodes)
+            {
+                var productName = productNode.QuerySelector(".search-link")?.TextContent.Trim();
+                var productLink = productNode.QuerySelector("a.search-link")?.GetAttribute("href");
+
+                if (!string.IsNullOrEmpty(productName) && !string.IsNullOrEmpty(productLink))
+                {
+                    var product = new Product
+                    {
+                        ProductId = Guid.NewGuid().ToString(),
+                        Name = productName,
+                        Description = null,
+                        Image = null,
+                        Link = productLink,
+                        ProductType = null
+                    };
+
+                    productList.Add(product);
+                }
+            }
+
             numberOfPage++;
         }
         
-        List<Product> products = productList.Select(name => new Product
+        List<Product> products = productList.Select(product => new Product
         {
-            ProductId = Guid.NewGuid().ToString(),
-            Name = name,
+            ProductId = product.ProductId,
+            Name = product.Name,
             Description = null,
             Image = null,
+            Link = product.Link,
             ProductType = null
         }).ToList();
 
